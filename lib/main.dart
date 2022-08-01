d0fd4c62a7ede6b8b6c05bb5a62a9557d5b72671
FlutLab.io <>
2022-08-01 18:00:07 +0000

    Autosave

--- lib/main.dart
+++ lib/main.dart
@@ -7,15 +7,9 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      // Application name
-      title: 'Flutter Stateful Clicker Counter',
-      theme: ThemeData(
-        // Application theme data, you can set the colors for the application as
-        // you want
-        primarySwatch: Colors.blue,
-      ),
-      home: MyHomePage(title: 'Flutter Demo Clicker Counter Home Page'),
-    );
+        // Application name
+
+        );
   }
 }
 
